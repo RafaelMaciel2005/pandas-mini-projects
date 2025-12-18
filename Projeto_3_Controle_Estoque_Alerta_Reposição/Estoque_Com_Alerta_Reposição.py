65ba6bbd7fc76090d167ca5b13042b585e0cdaeb
Rafael Maciel <maciel.rafael2005@gmail.com>
2025-12-18 16:06:22 -0300

    att

--- Projeto_3_Controle_Estoque_Alerta_Reposição/Estoque_Com_Alerta_Reposição.py
+++ Projeto_3_Controle_Estoque_Alerta_Reposição/Estoque_Com_Alerta_Reposição.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from IPython.display import display
 
-
+#Criando Data Frame
 dados_estoque = pd.DataFrame({
 
     "Produto": ["Teclado", "Mouse", "Monitor", "HeadSet", "Webcam"],
@@ -10,4 +10,32 @@ dados_estoque = pd.DataFrame({
 
 })
 
+#Salvando arquivo
 dados_estoque.to_csv("arquivos_projetos/dados_estoque.csv", index = False)
+
+#Criando a coluna Valor_Total.
+dados_estoque["Valor_Total"] = dados_estoque["Quantidade"]*dados_estoque["Preco_Unitario"]
+
+#Criando a coluna STATUS.
+estoque_minimo = 10
+
+dados_estoque["Status"] = "OK"
+dados_estoque.loc[dados_estoque["Quantidade"] < estoque_minimo, "Status"] = "REPOR"
+
+#Filtrando produtos que precisam de reposição e que estão OK.
+produtos_repor = dados_estoque[dados_estoque["Status"] == "REPOR"]
+produtos_OK = dados_estoque[dados_estoque["Status"] == "OK"]
+
+
+#Salvando arquivo após tratamento de dados.
+dados_estoque.to_csv("arquivos_projetos/dados_estoque_Final.csv", index = False)
+
+#Exibindo resultados.
+print("ESTOQUE COMPLETO:")
+display(dados_estoque)
+
+print("\n PRODUTOS PARA REPOSIÇÃO:")
+display(produtos_repor)
+
+print("\nProdutos que estão OK:")
+display(produtos_OK)
