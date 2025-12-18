8c5ca9a806976fdd4094ebf5e68636ac2b2838ab
Rafael Maciel <maciel.rafael2005@gmail.com>
2025-12-17 21:34:28 -0300

    V1.0

--- Projeto 1/Análise_funcionários.py
+++ Projeto 1/Análise_funcionários.py
@@ -1,34 +1,30 @@
-"""
-1 - gere um DataFrame com informações de funcionários contendo:
-        - nome
-        -departamento
-        -idade
-        -salário
-
-2 - Salve esse DataFrame em um arquivo CSV dentro da pasta data/.
-
-3 - Leia o arquivo CSV usando Pandas.
-
-    Calcule:
-
-        O salário médio por departamento
-
-        O funcionário com o maior salário
-
-        A idade média dos funcionários
-"""
 import pandas as pd
 from IPython.display import display
 
-#criando DataFrame
+#criando DataFrame.
 funcionarios = pd.DataFrame({
 
     "Nome": ["Rafael", "Ronney", "Denira", "Leoni"],
-    "Departamento": ["Financeiro", "RH", "Gerente", "Vendedor"],
+    "Departamento": ["Financeiro", "RH", "Gerente", "Financeiro"],
     "Idade": [20, 30, 53, 66],
     "Salário": [5500.00, 4290.00, 7640.00, 3200.00]
 
 })
 
-#salvando em CSV
+#salvando em CSV.
 funcionarios.to_csv("arquivos_projetos/funcionarios.csv", index = False)
+
+#lendo o DataFrame.
+func = pd.read_csv("arquivos_projetos/funcionarios.csv")
+
+#Calculando a média de cada departamento.
+media_dep = func.groupby("Departamento")["Salário"].mean()
+display(f"média salarial por departamento: {media_dep}:")
+
+#O funcionário com o maior salário.
+func_maior_salario = func.loc[func["Salário"].idxmax()]
+display(f"Maior Salário: {func_maior_salario}")
+
+#Descobrindo a idade media dos funcionarios.
+idade_media_funcionarios = func["Idade"].mean()
+display(f"Idade media dos funcionarios: {idade_media_funcionarios}")
